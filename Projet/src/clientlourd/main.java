906d491ea7ba8bd11692b98b1229c5ee9c7a2889
jouvetlucas@yahoo.fr <jouvetlucas@yahoo.fr>
2018-05-02 15:25:05 +0200

    stabilisation

--- Projet/src/clientlourd/main.java
+++ Projet/src/clientlourd/main.java
@@ -8,19 +8,18 @@ import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 public class main {
 
 	public static void main(String[] args) {
-		//GestionMessages chat = GestionMessages.get(); 
-	      // Start all Swing applications on the EDT.
-	      SwingUtilities.invokeLater(new Runnable() {
-	         public void run() {
-	            try {
-	               String laf = UIManager.getSystemLookAndFeelClassName();
-	               UIManager.setLookAndFeel(laf);
-	            } catch (Exception e) { /* never happens */ }
-	            editeur demo = new editeur(null,SyntaxConstants.SYNTAX_STYLE_C);
-	            demo.setVisible(true);
-	            demo.gettextarea().requestFocusInWindow();
-	         }
-	      });
-	   }
-
+	 //GestionMessages chat = GestionMessages.get(); 
+	 // Start all Swing applications on the EDT.
+	 SwingUtilities.invokeLater(new Runnable() {
+		 public void run() {
+		  try {
+		    String laf = UIManager.getSystemLookAndFeelClassName();
+		    UIManager.setLookAndFeel(laf);
+		    } catch (Exception e) { /* never happens */ }
+		  editeur demo = new editeur(null,SyntaxConstants.SYNTAX_STYLE_C);
+		  demo.setVisible(true);
+		  demo.gettextarea().requestFocusInWindow();
+		  }
+		 });
+	}
 }
