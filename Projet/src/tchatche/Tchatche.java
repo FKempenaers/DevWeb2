deee4d01433a3aa6bc8cf9ccb9a2269b12cbd96f
fkempenaers <francis.kempenaers@gmail.com>
2018-05-02 15:43:02 +0200

    stab4

--- Projet/src/tchatche/Tchatche.java
+++ Projet/src/tchatche/Tchatche.java
@@ -38,7 +38,6 @@ public class Tchatche extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		if(is empty)
 		
 		HttpSession session = request.getSession();
 		
