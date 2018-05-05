a48c7abc59cc5f55c6af706d1d423dbc29e30788
jouvetlucas@yahoo.fr <jouvetlucas@yahoo.fr>
2018-05-05 11:28:23 +0200

    pakage Server

--- Projet/src/clientlourd/Administration.java
+++ Projet/src/clientlourd/Administration.java
@@ -21,7 +21,9 @@ public class Administration extends JFrame{
 	private JPanel cp;
 	private JLabel[] fichiers;
 	private JButton[] bAF;
-	private JButton allFi;
+	private JButton[] bAFA;
+	private JButton preced,suivant;
+	private int n;
 	
 	public Administration() {
 		
@@ -32,6 +34,7 @@ public class Administration extends JFrame{
 		lf.setSize(200, 13);
 		lf.setText("Liste des fichiers");
 		cp.add(lf);
+		n = 0;
 		affiche_fichiers();
 		
 		
@@ -46,17 +49,33 @@ public class Administration extends JFrame{
 		int j = 40;
 		fichiers = new JLabel[20];
 		bAF = new JButton[20];
-		for(int i = 0; i < 20;i++,j+=20) {
-			fichiers[i] = new JLabel();
-			fichiers[i].setLayout(null);
-			fichiers[i].setLocation(new Point(0,j));
-			fichiers[i].setSize(100, 17);
-			fichiers[i].setText("fichier "+i);
+		bAFA = new JButton[20];
+		for(int i = n; i < n+20;i++,j+=20) {
+			fichiers[i-n] = new JLabel();
+			fichiers[i-n].setLayout(null);
+			fichiers[i-n].setLocation(new Point(0,j));
+			fichiers[i-n].setSize(100, 17);
+			fichiers[i-n].setText("fichier "+i);
 			
-			bAF[i] = new JButton("Editer "+i);
-			bAF[i].setBounds(110, j, 80, 20);
+			bAFA[i-n] = new JButton("Admin "+i);
+			bAFA[i-n].setBounds(260,j,100,20);
+			
+			bAFA[i-n].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					JLabel membres = new JLabel();
+					membres.setLayout(null);
+					membres.setLocation(new Point(400,20));
+					membres.setSize(300, 17);
+					membres.setText("Membres du fichier");
+					cp.add(membres);
+					repaint();
+				}
+			});
+			cp.add(bAFA[i-n]);
+			bAF[i-n] = new JButton("Editer "+i);
+			bAF[i-n].setBounds(110, j, 150, 20);
 
-			bAF[i].addActionListener(new ActionListener() {
+			bAF[i-n].addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
 					SwingUtilities.invokeLater(new Runnable() {
 					  public void run() {
@@ -86,33 +105,42 @@ public class Administration extends JFrame{
 				}
 			});
 			
-			
-			
-			
-			cp.add(fichiers[i]);
-			cp.add(bAF[i]);
+			cp.add(fichiers[i-n]);
+			cp.add(bAF[i-n]);
 			
 		}
-		allFi = new JButton("Tous les fichiers");
-		allFi.setBounds(0, j+40, 190, 20);
-		allFi.addActionListener(new ActionListener() {
+		preced = new JButton("Precedant");
+		preced.setBounds(10, j+40, 100, 20);
+		preced.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				SwingUtilities.invokeLater(new Runnable() {
-				  public void run() {
-					try {
-						String laf = UIManager.getSystemLookAndFeelClassName();
-						UIManager.setLookAndFeel(laf);
-					} catch (Exception e) { /* never happens */ }
-					AllFiles af = new AllFiles();
-					
-					af.setSize(400, 600);
-					af.setVisible(true);
-					af.requestFocus();
-				 }
-			    });
+				n -= 20;
+				if(n < 0) {
+					n = 0;
+				}
+				for(int i = 0;i<20;i++) {
+				  remove(bAF[i]);
+				  remove(fichiers[i]);
+				}
+				affiche_fichiers();
+				repaint();
 			}
 		});
-		cp.add(allFi);
+		cp.add(preced);
+		
+		suivant= new JButton("Suivant");
+		suivant.setBounds(110, j+40, 100, 20);
+		suivant.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				n += 20;
+				for(int i = 0;i<20;i++) {
+					  remove(bAF[i]);
+					  remove(fichiers[i]);
+				}
+				affiche_fichiers();
+				repaint();
+			}
+		});
+		cp.add(suivant);
 	}
 
 	
