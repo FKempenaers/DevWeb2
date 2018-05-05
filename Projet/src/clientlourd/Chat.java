7212b699d7d66775ca70153dbfdc3d597665df86
jouvetlucas@yahoo.fr <jouvetlucas@yahoo.fr>
2018-05-05 13:52:05 +0200

    lol

--- Projet/src/clientlourd/Chat.java
+++ Projet/src/clientlourd/Chat.java
@@ -4,6 +4,13 @@ package clientlourd;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -103,12 +110,12 @@ public class Chat extends JFrame {
 	public void actumessage() {
 		int nbmessage;
 		String m;
-		nbmessage = chat.nbmessage()-23;
+		nbmessage = snbmessage()-23;
 		if(nbmessage < 0) {
 			nbmessage = 0;
 		}
 		for(int i = 0; i < 24; i++) {
-			m = chat.afficherClientLourd(nbmessage);
+			m = message(nbmessage);
 			if(m != "Message non existant") {
 			 text[i].setText(m);
 			}
@@ -117,5 +124,39 @@ public class Chat extends JFrame {
 			}
 			nbmessage++;
 		}
+	}
+	public int snbmessage() {
+		final int PORT = 8888;
+		try {
+			Socket s = new Socket("localhost",PORT);
+			InputStream in = s.getInputStream();
+			OutputStream out = s.getOutputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			PrintWriter writer = new PrintWriter(out);
+			writer.print("nbmessages");
+			writer.flush();
+			return Integer.parseInt(reader.readLine());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	public String message(int n) {
+		final int PORT = 8888;
+		try {
+			Socket s = new Socket("localhost",PORT);
+			InputStream in = s.getInputStream();
+			OutputStream out = s.getOutputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			PrintWriter writer = new PrintWriter(out);
+			writer.print("n");
+			writer.flush();
+			return reader.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
--- Projet/src/serveur/Serveur.java
+++ Projet/src/serveur/Serveur.java
@@ -1,5 +1,34 @@
 package serveur;
 
-public class Serveur {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import tchatche.GestionMessages;
+
+public class Serveur {
+	private GestionMessages chat;
+	public Serveur() {
+		final int PORT = 8888;
+		try {
+			ServerSocket server = new ServerSocket(PORT, 1);
+			
+			Socket s = server.accept();
+			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			PrintWriter out = new PrintWriter(s.getOutputStream());
+			String ligne;
+			while(true) {
+				ligne = in.readLine();
+				if(ligne.equals("nbmessages")) {
+					out.println(chat.nbmessage());
+					out.flush();
+				}
+				out.println(chat.afficherClientLourd(Integer.parseInt(ligne)));
+				out.flush();
+			}
+		} catch (IOException e) {}
+	}
 }
